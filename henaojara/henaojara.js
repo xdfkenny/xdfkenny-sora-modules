0a3fd70a6f9213b9d0a2cb6b865ab00c0cbf3393
xdfkenny <xdfkenny@gmail.com>
2025-03-19 16:50:04 -0400

    Update henaojara.js

--- henaojara/henaojara.js
+++ henaojara/henaojara.js
@@ -1,12 +1,17 @@
 async function searchResultsHenaojara(keyword) {
     try {
+        // Encode the keyword for use in the URL
         const encodedKeyword = encodeURIComponent(keyword);
+
+        // Fetch the search results page
         const response = await fetch(`https://henaojara.com/?s=${encodedKeyword}`);
         const responseText = await response.text(); // Get HTML as text
 
+        // Parse the HTML into a DOM object
         const parser = new DOMParser();
         const doc = parser.parseFromString(responseText, "text/html");
 
+        // Extract results from the DOM
         const transformedResults = [...doc.querySelectorAll("li.TPostMv")].map(item => {
             const titleElement = item.querySelector("h3.Title");
             const imageElement = item.querySelector("img");
@@ -19,10 +24,13 @@ async function searchResultsHenaojara(keyword) {
             };
         });
 
+        // Return the results as a JSON string
         return JSON.stringify(transformedResults);
 
     } catch (error) {
         console.log("Fetch error:", error);
+
+        // Return an error object as a JSON string
         return JSON.stringify([{ title: "Error", image: "", href: "" }]);
     }
 }
